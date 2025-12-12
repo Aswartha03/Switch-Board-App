4d10122f54b7ce2babbe4f4aa337c5651e9edc6b
Badigi Aswartha Reddy <aswartha.b@Keka.com>
2025-12-12 15:48:53 +0530

    Switch Board

--- Switch Board App/Devices/AC.cs
+++ Switch Board App/Devices/AC.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Switch_Board_App.Interfaces;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Switch_Board_App.Interfaces;
 
-namespace Switch_Board_App.Devices
-{
-    internal class AC : IDevice
-	{
+//namespace Switch_Board_App.Devices
+//{
+//    internal class AC : IDevice
+//	{
 
-		public string Name { get; set; } = "AC";
-		public bool IsDeviceOn { get; set; } = false;
-		public void TurnOff()
-		{
-			IsDeviceOn = false;
-		}	
-		public void TurnOn()
-		{
-			IsDeviceOn = true;
-		}
+//		public string Name { get; set; } = "AC";
+//		public bool IsDeviceOn { get; set; } = false;
+//		public void TurnOff()
+//		{
+//			IsDeviceOn = false;
+//		}	
+//		public void TurnOn()
+//		{
+//			IsDeviceOn = true;
+//		}
 
-		public string DisplayStatus()
-		{
-			string status = IsDeviceOn ? "On" : "Off";
-			return $"AC is {status}";
-		}
-	}
-}
+//		public string DisplayStatus()
+//		{
+//			string status = IsDeviceOn ? "On" : "Off";
+//			return $"AC is {status}";
+//		}
+//	}
+//}
--- Switch Board App/Devices/Fan.cs
+++ Switch Board App/Devices/Fan.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Switch_Board_App.Interfaces;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Switch_Board_App.Interfaces;
 
-namespace Switch_Board_App.Devices
-{
-    internal class Fan : IDevice
-    {
+//namespace Switch_Board_App.Devices
+//{
+//    internal class Fan : IDevice
+//    {
 
-        public string Name { get; set; } = "Fan";   
-		public bool IsDeviceOn { get; set; } = false;
+//        public string Name { get; set; } = "Fan";   
+//		public bool IsDeviceOn { get; set; } = false;
 
-		public void TurnOff() 
-        {
-            IsDeviceOn = false;
-		}
+//		public void TurnOff() 
+//        {
+//            IsDeviceOn = false;
+//		}
 
-        public void TurnOn()
-        {
-            IsDeviceOn = true; 
-		}
-        public string DisplayStatus()
-        {
-            string status = IsDeviceOn ? "On" : "Off";
-            return $"Fan is {status}";
-		}
-	}
-}
+//        public void TurnOn()
+//        {
+//            IsDeviceOn = true; 
+//		}
+//        public string DisplayStatus()
+//        {
+//            string status = IsDeviceOn ? "On" : "Off";
+//            return $"Fan is {status}";
+//		}
+//	}
+//}

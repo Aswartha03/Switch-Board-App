5e608a20abd42ab21cb027ab119ae9cb61a3f13f
aswartha.b <aswartha.b@L-DEV-0384>
2025-12-12 12:09:36 +0530

    Add project files.

---
+++ Switch Board App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Switch_Board_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
